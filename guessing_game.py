bf4c9b1c1c527ecdb29d540c70d593640f3217af
Michael Clermont <michael.clermont@mths.ca>
2022-03-31 18:43:08 +0000

    added guessing game program

--- guessing_game.py
+++ guessing_game.py
@@ -11,15 +11,20 @@ def main():
     # this function shows formatting output
 
     # input
-    number_from_user = int(input("Enter a number between 0-9: "))
+    number_as_string = input("Enter a number between 0-9: ")
     random_number = random.randint(1, 9)
     # process & output
     print("")
-    if number_from_user == random_number:
-        print("You guessed correctly!")
-    else:
-        print("You guessed incorrectly!")
-    print("\nDone.")
+    try:
+        number_as_integer = int(number_as_string)
+        if number_as_integer == random_number:
+            print("You guessed correctly!")
+        else:
+            print("You guessed incorrectly!")
+    except Exception:
+        print("That is not an integer.")
+    finally:
+        print("\nDone.")
 
 
 if __name__ == "__main__":
